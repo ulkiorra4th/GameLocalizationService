fb8234d8ebc2564da6d741444957b560b69d0278
Рамин Мухтаров <rm@crmpark.ru>
2025-07-07 06:14:55 +0300

    Add project data

--- backend/GameLocalization/GameLocalization.Persistence.Postgres/Configurations/ProjectConfiguration.cs
+++ backend/GameLocalization/GameLocalization.Persistence.Postgres/Configurations/ProjectConfiguration.cs
@@ -15,12 +15,29 @@ internal sealed class ProjectConfiguration : IEntityTypeConfiguration<ProjectEnt
         builder.Property(p => p.CreatedAt).IsRequired();
         builder.Property(p => p.UpdatedAt).IsRequired();
 
-        builder.HasData(new ProjectEntity
-        {
-            Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
-            Name = "Demo Project",
-            CreatedAt = DateTime.UtcNow,
-            UpdatedAt = DateTime.UtcNow
-        });
+        builder.HasData(new List<ProjectEntity>
+                {
+                    new ()
+                    {
+                        Id = Guid.Parse("11111111-1111-1111-1111-111111111111"),
+                        Name = "Demo Project",
+                        CreatedAt = DateTime.UtcNow,
+                        UpdatedAt = DateTime.UtcNow
+                    },
+                    new ()
+                    {
+                        Id = Guid.Parse("22222222-2222-2222-2222-222222222222"),
+                        Name = "Demo Project 2",
+                        CreatedAt = DateTime.UtcNow,
+                        UpdatedAt = DateTime.UtcNow
+                    },
+                    new ()
+                    {
+                        Id = Guid.Parse("33333333-3333-3333-3333-333333333333"),
+                        Name = "Demo Project 3",
+                        CreatedAt = DateTime.UtcNow,
+                        UpdatedAt = DateTime.UtcNow
+                    }
+                });
     }
 }
